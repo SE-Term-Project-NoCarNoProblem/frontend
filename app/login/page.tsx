13bd9361409e8b5a0399126fe6b55f767fe5ecdf
Akaporn <150357038+Akaporn@users.noreply.github.com>
2025-09-02 19:09:44 +0700

    feat: implement user login page v.2

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -8,6 +8,21 @@ export default function LoginPage() {
   const router = useRouter();
   const themeColor = "#0E4663";
   const [showPassword, setShowPassword] = useState(false);
+  const [form, setForm] = useState({
+    email: "",
+    password: ""
+  });
+
+    function handelChange(e:any){
+        console.log(e.target.value);
+        setForm({...form, [e.target.name]: e.target.value}); 
+    }
+
+    function handleSubmit(e:any){
+        e.preventDefault(); 
+        console.log(`email : `, form.email);
+        console.log(`password : `, form.password);  
+    }
 
   return (<>
     <div className="bg-white p-12 flex flex-col items-center text-[#000000]">
@@ -24,45 +39,52 @@ export default function LoginPage() {
                 </button>
             </p>
 
-            {/* Form */}
-            <form className="mt-6 space-y-4">
-                {/* Email */}
+            {/* ---------------- Form ---------------- */}
+            <form className="mt-6 space-y-4" onSubmit={handleSubmit}>
+                {/* ---------------- Email ---------------- */}
                 <p className={`text-[${themeColor}] my-0`}> Username </p>
                 <input
                     type="email"
                     placeholder="Email address"
                     className="w-full p-2 border rounded-md"
+                    name="email"
+                    value={form.email}
+                    onChange={handelChange}
                 />
 
-                {/* Password */}
+                {/* ---------------- Password ---------------- */}
                 <p className={`text-[${themeColor}] my-0`} > Password</p>
                 <div className="flex gap-4">
                     <input
                         type={showPassword ? "text" : "password"}
                         placeholder="Password"
                         className="w-1/2 p-2 border rounded-md w-full"
+                        name="password"
+                        value={form.password}
+                        onChange={handelChange}
                     />
                 </div>
+
+                 {/* ---------------- Show password toggle ---------------- */}
+                <label className={`flex items-center gap-2 text-sm my-1 mb-4 text-[${themeColor}]`}>
+                    <input 
+                        type="checkbox"
+                        checked={showPassword}
+                        onChange={() => setShowPassword(!showPassword)}
+                    />
+                    Show password
+                </label>
+
+                {/* ---------------- Submit botton ---------------- */}
+                <button
+                    type="submit"
+                    className={`w-full bg-[${themeColor}] text-white p-2 rounded-md hover:bg-[${themeColor}]/90 hover:cursor-pointer`}
+                >
+                    Log in
+                </button>
             </form>
-
-            {/* Show password toggle */}
-            <label className={`flex items-center gap-2 text-sm my-1 mb-4 text-[${themeColor}]`}>
-                <input 
-                    type="checkbox"
-                    checked={showPassword}
-                    onChange={() => setShowPassword(!showPassword)}
-                />
-                Show password
-            </label>
-
-            {/* Submit botton */}
-            <button
-                className={`w-full bg-[${themeColor}] text-white p-2 rounded-md hover:bg-[${themeColor}]/90 hover:cursor-pointer`}
-            >
-                Log in
-            </button>
-
         </div>
+
         <div className="flex-1 flex justify-center ">
             <Image
                 aria-hidden 
