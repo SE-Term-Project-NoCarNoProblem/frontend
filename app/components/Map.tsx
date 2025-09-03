88481615857220a8e9470e990e0210194d308bb4
Thitiwut-Win <bmwin9825@gmail.com>
2025-09-03 14:24:43 +0700

    Added Map components

---
+++ app/components/Map.tsx
@@ -0,0 +1,21 @@
+'use client'
+import { MapContainer, Marker, Popup, TileLayer, Tooltip } from "react-leaflet"
+import "leaflet/dist/leaflet.css"
+import "leaflet-defaulticon-compatibility"
+import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
+
+export default function Map(props: any) {
+  const { position, zoom } = props
+
+  return <MapContainer center={[13.7563, 100.5018]} zoom={13} className="h-[100vh]">
+    <TileLayer
+      attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+      url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+    />
+    <Marker position={[13.7563, 100.5018]}>
+      <Popup>
+        A pretty CSS3 popup. <br /> Easily customizable.
+      </Popup>
+    </Marker>
+  </MapContainer>
+}
