f0a3394940a48c1c0f56fcc7154b4c9facf53147
Thitiwut-Win <bmwin9825@gmail.com>
2025-09-10 21:31:07 +0700

    Added and split pickup and destination

--- app/components/Map.tsx
+++ app/components/Map.tsx
@@ -5,60 +5,91 @@ import "leaflet-defaulticon-compatibility"
 import "leaflet-defaulticon-compatibility/dist/leaflet-defaulticon-compatibility.css"
 import { useEffect, useState } from "react"
 import { RecenterMap } from "@/app/hooks/RecenterMap"
+import { useGeolocation } from '@vueuse/core'
+
 
 export default function Map(props: any) {
+  //from location page
   const { position, zoom = 10 } = props
 
-  const [marker, setMarker] = useState<[number, number]>(position);
-  const [address, setAddress] = useState<string>("");
-  const [query, setQuery] = useState("");
+  const [srcMarker, setSrcMarker] = useState<[number, number]>(position);
+  const [srcAddress, setSrcAddress] = useState<string>("");
+  const [srcQuery, setSrcQuery] = useState("");
 
+  const [destMarker, setDestMarker] = useState<[number, number] | null>(null);
+  const [destAddress, setDestAddress] = useState<string>("");
+  const [destQuery, setDestQuery] = useState("");
+  //for the map to recenetr
+  const [lastChanged, setLastChanged] = useState<"src" | "dest" | null>(null);
+  //for map clicked
+  const [lastFocused, setLastFocused] = useState<"src" | "dest" | null>(null);
+  //vueuse location permission
+  const { coords, locatedAt, error, resume, pause } = useGeolocation()
+
+  /* ### Get source/pickup location and update ### */
   useEffect(() => {
-  if ("geolocation" in navigator) {
-    navigator.geolocation.getCurrentPosition(
-      (pos) => {
-        const { latitude, longitude } = pos.coords;
-        setMarker([latitude, longitude]);
-      },
-      (err) => {
-        console.warn("Geolocation error:", err);
-      }
-    );
-  }
-}, []);
-
-  useEffect(() => {
-    if (!marker) return;
-
-    const fetchAddress = async () => {
-      try {
-        const [lat, lng] = marker;
-        const res = await fetch(
-          `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json` //Usage limit: 1 request/sec per IP
-        );
-        const data = await res.json();
-        setAddress(data.display_name || "Unknown location");
-        setQuery(data.display_name || "Unknown location");
-      } catch (err) {
-        setAddress("Error fetching address");
-      }
-    };
-
-    fetchAddress();
-  }, [marker]);
-
-  function OnMapClicked() {
-    useMapEvents({
-        click: (e)=>{
-            console.log(e)
-            const { lat, lng } = e.latlng;
-            setMarker([lat, lng])
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (pos) => {
+          const { latitude, longitude } = pos.coords;
+          setSrcMarker([latitude, longitude]);
+          setLastChanged("src");
+        },
+        (err) => {
+          console.error("Geolocation error:", err);
         }
+      );
+    }
+  }, []);
+
+  /* ### Translate coord to address(reverse) ### */
+  async function reverseGeocode(lat: number, lng: number): Promise<string> {
+    const res = await fetch(
+      `https://nominatim.openstreetmap.org/reverse?lat=${lat}&lon=${lng}&format=json`
+    );
+    const data = await res.json();
+    return data.display_name;
+  }
+  //src
+  useEffect(() => {
+    if (!srcMarker) return;
+
+    const [lat, lng] = srcMarker;
+    reverseGeocode(lat, lng).then((display_name) => {
+      setSrcAddress(display_name);
+      setSrcQuery(display_name);
+    });
+  }, [srcMarker]);
+  //dest
+  useEffect(() => {
+    if (destMarker) {
+      const [lat, lng] = destMarker;
+      reverseGeocode(lat, lng).then((display_name) => {
+        setDestAddress(display_name);
+        setDestQuery(display_name);
+      });
+    }
+  }, [destMarker]);
+
+  function OnMapClicked({type} : {type:string}) {
+    useMapEvents({
+      click: (e)=>{
+        console.log(e)
+        const { lat, lng } = e.latlng;
+        if (type === "src") {
+          setSrcMarker([lat, lng])
+          setLastChanged("src");
+        } else if (type === "dest") {
+          setDestMarker([lat, lng])
+          setLastChanged("dest");
+        }
+      }
     })
     return null;
   }
 
-  async function handleSearch(e: React.FormEvent) {
+  /* ### Translate address to coord(forward) ### */
+  async function handleSearch(e: React.FormEvent, type: "src" | "dest",  query: string) {
     e.preventDefault();
     if (!query) return;
 
@@ -73,47 +104,93 @@ export default function Map(props: any) {
       if (data.length > 0) {
         const { lat, lon, display_name } = data[0];
         const coords: [number, number] = [parseFloat(lat), parseFloat(lon)];
-        setMarker(coords);
-        setAddress(display_name);
+        if (type === "src") {
+          setSrcMarker(coords);
+          setSrcAddress(display_name);
+        } else if (type === "dest") {
+          setDestMarker(coords);
+          setDestAddress(display_name);
+        }
+        setLastChanged(type);
       } else {
-        setAddress("Location not found");
+        if (type === "src") {
+          setSrcAddress("Location not found");
+        } else if (type === "dest") {
+          setDestAddress("Location not found");
+        }
       }
     } catch (err) {
-      setAddress("Error searching location");
+      if (type === "src") {
+        setSrcAddress("Error searching location");
+      } else if (type === "dest") {
+        setDestAddress("Error searching location");
+      }
     }
   }
 
   return (
-      <div className="flex flex-col h-screen">
-      <form onSubmit={handleSearch} className="p-2 flex gap-2 bg-slate-300">
-        <input
-          type="text"
-          value={query}
-          placeholder="Enter a place"
-          onChange={(e) => setQuery(e.target.value)}
-          className="flex-1 p-2 border rounded-xl text-black placeholder-slate-600 focus:outline-none 
-          focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
+    <div className="flex flex-col h-screen">
+      <div className="flex flex-row w-full">
+        <form onSubmit={(e) => handleSearch(e, "src", srcQuery)} 
+        className="flex-1 p-2 flex gap-2 bg-slate-300 border border-red-500 border-1 hover:border-3"
+        onFocus={()=>{setLastFocused("src")}}
+        >
+          <div className="text-emerald-600 text-xl my-auto">Pickup</div>
+          <input
+            type="text"
+            value={srcQuery}
+            placeholder="Enter a place"
+            onChange={(e) => setSrcQuery(e.target.value)}
+            className="flex-1 p-2 border rounded-xl text-black placeholder-slate-600 focus:outline-none 
+            focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
+          />
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-indigo-500 
+          hover:shadow-lg hover:-translate-y-1 hover:scale-110">
+            Search
+          </button>
+        </form>
+        <form onSubmit={(e) => handleSearch(e, "dest", destQuery)} 
+        className="flex-1 p-2 flex gap-2 bg-slate-300 border border-red-500 border-1 hover:border-3"
+        onFocus={()=>{setLastFocused("dest")}}
+        >
+          <div className="text-emerald-600 text-xl my-auto">Destination</div>
+          <input
+            type="text"
+            value={destQuery}
+            placeholder="Enter a place"
+            onChange={(e) => setDestQuery(e.target.value)}
+            className="flex-1 p-2 border rounded-xl text-black placeholder-slate-600 focus:outline-none 
+            focus:border-orange-500 focus:ring-1 focus:ring-orange-500"
+          />
+          <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-indigo-500 
+          hover:shadow-lg hover:-translate-y-1 hover:scale-110">
+            Search
+          </button>
+        </form>
+      </div>
+      <MapContainer center={position} zoom={zoom} className="h-[100vh]">
+        <TileLayer attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
-        <button type="submit" className="px-4 py-2 bg-blue-500 text-white rounded-lg hover:bg-indigo-500 
-        hover:shadow-lg hover:-translate-y-1 hover:scale-110">
-          Search
-        </button>
-      </form>
-    <MapContainer center={position} zoom={zoom} className="h-[100vh]">
-        <TileLayer
-            attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-            url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-        />
-        {marker && (
+        {srcMarker && (
           <>
-            <Marker position={marker}>
-              <Popup><b>Address:</b> {address}</Popup>
+            <Marker position={srcMarker}>
+              <Popup><b>Address:</b> {srcAddress}</Popup>
             </Marker>
-            <RecenterMap position={marker} />
           </>
         )}
-        <OnMapClicked />
-    </MapContainer>
+        {destMarker && (
+          <>
+            <Marker position={destMarker}>
+              <Popup><b>Destination:</b> {destAddress}</Popup>
+            </Marker>
+          </>
+        )}
+        {lastChanged === "src" && srcMarker && <RecenterMap position={srcMarker} />}
+        {lastChanged === "dest" && destMarker && <RecenterMap position={destMarker} />}
+        {lastFocused === "src" && <OnMapClicked type="src" />}
+        {lastFocused === "dest" && <OnMapClicked type="dest" />}
+      </MapContainer>
     </div>
   );
 }
--- app/hooks/RecenterMap.tsx
+++ app/hooks/RecenterMap.tsx
@@ -1,13 +1,10 @@
-import { useEffect } from "react";
 import { useMap } from "react-leaflet";
 
-export function RecenterMap({ position }: { position: [number, number] }) {
+export function RecenterMap({ position } : { position: [number, number] }) {
     const map = useMap();
 
-    useEffect(() => {
-        if (!position || position.length !== 2) return;
-        map.setView(position, 13);
-    }, [position, map]);
+    if (!position || position.length !== 2) return;
+    map.setView(position, 10);
 
     return null;
 }
