20db76f6a8e72e476390f25208086af4187ced90
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-09-13 16:57:08 +0700

    feat: implement ride request list for driver screen 1

--- app/ride-request/page.tsx
+++ app/ride-request/page.tsx
@@ -2,6 +2,7 @@
 
 import { useEffect, useMemo, useState } from "react";
 import dynamic from "next/dynamic";
+import Image from "next/image";
 
 export default function RideRequest() {
   const Map = useMemo(() => dynamic(
@@ -20,14 +21,45 @@ export default function RideRequest() {
   return (
   <div className="relative w-full h-screen">
       {/* Map fills the parent */}
-      <Map position={position}/>
+    <Map position={position}/>
 
-  {/* Bottom panel (always sticks to bottom of viewport) */}
-  <div className="absolute bottom-0 left-0 w-full z-400 bg-white p-4 shadow-lg rounded-t-2xl">
-    <button className="w-full bg-blue-600 text-white py-2 rounded-lg">
-      Confirm Ride
-    </button>
-  </div>
+    {/* Bottom panel (always sticks to bottom of viewport) */}
+    <div className="absolute bottom-0 left-0 w-full z-400 bg-white p-4 shadow-lg rounded-2xl text-[#0E4663]">
+        <Image
+            src="/arrow_back.svg"
+            alt="arrow"
+            width={20}
+            height={20}
+            className="rounded-full"
+        />
+        
+        <div className="flex w-[100%] justify-between mx-2 items-center px-4">
+            <div className=""> 
+                <div className="font-semibold">
+                    Sippakorn Thunyahan 
+                </div>
+                <div className="text-sm">
+                    Honda civic (Black) : กข 1234 
+                </div>      
+            </div>
+            <div className="">
+                <Image
+                    src="/Avatar.svg"
+                    alt="Avatar"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                />     
+                <Image
+                    src="/rating.svg"
+                    alt="rating"
+                    width={50}
+                    height={50}
+                    className="rounded-full"
+                />    
+            </div>
+        </div>
+    </div>
   </div>
   )
 }
