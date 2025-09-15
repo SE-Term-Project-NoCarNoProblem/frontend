677396d7114c7c202893f4d06a6ad5d798ef34ce
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-09-15 21:46:27 +0700

    fix: fetch-ride

--- app/ride-request/page.tsx
+++ app/ride-request/page.tsx
@@ -4,6 +4,15 @@ import { useEffect, useMemo, useState } from "react";
 import dynamic from "next/dynamic";
 import Image from "next/image";
 
+interface Driver {
+  id: string;
+  name: string;
+  car: string;
+  plate: string;
+  rating: number;
+  avatarUrl: string;
+}
+
 export default function RideRequest() {
   const Map = useMemo(() => dynamic(
     () => import('@/app/components/Map'),
@@ -21,13 +30,28 @@ export default function RideRequest() {
     [13.7650, 100.5200]  // Random
   ]);
 
+  const [driver, setDriver] = useState<Driver | null>(null);
+    // Fetch ride request (mock ride id for now)
+  useEffect(() => {
+    const fetchRide = async () => {
+      try {
+        const res = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/users/nearby`);
+        const data = await res.json();
+        setDriver(data.driver); // store driver info
+      } catch (err) {
+        console.error("Failed to fetch ride:", err);
+      }
+    };
+    fetchRide();
+  }, []);
+
 useEffect(() => {
   const interval = setInterval(async () => {
     try {
       const position = await getDriverLocation();
 
       // Send to backend
-      console.log(position);
+      // console.log(position);
       await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/drivers/location`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -61,15 +85,18 @@ useEffect(() => {
         <div className="flex w-[100%] justify-between mx-2 items-center px-4">
             <div> 
                 <div className="font-semibold">
+                    {/* {driver.name} */}
                     Sippakorn Thunyahan 
                 </div>
                 <div className="text-sm">
+                    {/* {driver.car} : {driver.plate} */}
                     Honda civic (Black) : กข 1234 
                 </div>      
             </div>
             <div>
                 <Image src="/Avatar.svg" alt="Avatar" width={50} height={50} className="rounded-full"/>
                 <Image src="/rating.svg" alt="rating" width={50} height={50} className="rounded-full"/>    
+                {/* <div className="text-sm text-center">{driver.rating}  ⭐</div> */}
             </div>
         </div>
     </div>
