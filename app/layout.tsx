e69a0ea56cd138ad3c9a111419bcb602d031e0cd
Krissada Singhakachain <omsin_47@hotmail.com>
2025-10-20 23:30:18 +0700

    fix: attempt to fix hydration mismatch on <body>

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,9 +30,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${poppins.variable} antialiased`}
-      >
+      <body>
         {children}
       </body>
     </html>
