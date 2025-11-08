f3c4144863e51b9bde34636b613e4ead3036d717
Akaporn <150357038+Akaporn@users.noreply.github.com>
2025-11-08 11:34:14 +0700

    feat:add admin side bar

--- app/driver-approval/page.tsx
+++ app/driver-approval/page.tsx
@@ -2,6 +2,7 @@
 
 import DriverApprovalCard from "../components/DriverApprovalCard"
 import { useEffect, useState } from "react";
+import AdminSidebar from "../components/AdminSideBar";
 
 export default function DriverApprovals(){
     const [drivers, setDrivers] = useState([
@@ -10,6 +11,30 @@ export default function DriverApprovals(){
             driverProfile :"http://localhost:3000/driver-profile",
             idPic : "../../temp_image.svg",
             licensePic : "../../default_profile.webp"
+        },
+        {
+            driverId : "054133d-0604-42ba-af89-60f6cd732816",
+            driverProfile :"#",
+            idPic : "#",
+            licensePic : "#"
+        },
+        {
+            driverId : "054133d-0604-42ba-af89-60f6cd732816",
+            driverProfile :"#",
+            idPic : "#",
+            licensePic : "#"
+        },
+        {
+            driverId : "054133d-0604-42ba-af89-60f6cd732816",
+            driverProfile :"#",
+            idPic : "#",
+            licensePic : "#"
+        },
+        {
+            driverId : "054133d-0604-42ba-af89-60f6cd732816",
+            driverProfile :"#",
+            idPic : "#",
+            licensePic : "#"
         },
         {
             driverId : "054133d-0604-42ba-af89-60f6cd732816",
@@ -48,34 +73,39 @@ export default function DriverApprovals(){
     }
 
     return (
-        <div className="p-6">
-            <div className="text-[20px] text-lg font-semibold flex items-center gap-2 mb-4 text-[#0E4663]">
-                <span> 
-                    <img
-                        src={'../../stash_person-duotone.svg'}
-                        alt="human logo"
-                    />
-                </span>
-                Driver Approvals
+        <div className="py-6 bg-white min-h-screen flex">
+            <AdminSidebar/>
+            <div className="flex-1">
+                <div className="text-[20px] text-lg font-semibold flex items-center gap-2 mb-4 text-[#0E4663]">
+                    <span> 
+                        <img
+                            src={'../../stash_person-duotone.svg'}
+                            alt="human logo"
+                        />
+                    </span>
+                    Driver Approvals
+                </div>
+
+                <hr className="border border-gray-200 my-4"/>
+
+                <div className="h-170 overflow-y-scroll">
+                    {drivers.length === 0 ? (
+                        <div className="text-lg font-semibold flex items-center gap-2 mb-4 text-[#0E4663]"> All Drivers has been considered</div>
+                    ) :(
+                        drivers.map((driver) => (
+                            <DriverApprovalCard
+                                key = {driver.driverId}
+                                driverId = {driver.driverId}
+                                driverProfile = {driver.driverProfile}
+                                idPic = {driver.idPic}
+                                licensePic = {driver.licensePic}
+                                onApprove = {() => handleApprove(driver.driverId)}
+                                onReject = {() => handleReject(driver.driverId)}
+                            />
+                        )))
+                    }
+                </div>
             </div>
-
-            <hr className="border border-gray-200 my-4"/>
-
-            {drivers.length === 0 ? (
-                <div className="text-lg font-semibold flex items-center gap-2 mb-4 text-[#0E4663]"> All Drivers has been considered</div>
-            ) :(
-                drivers.map((driver) => (
-                    <DriverApprovalCard
-                        key = {driver.driverId}
-                        driverId = {driver.driverId}
-                        driverProfile = {driver.driverProfile}
-                        idPic = {driver.idPic}
-                        licensePic = {driver.licensePic}
-                        onApprove = {() => handleApprove(driver.driverId)}
-                        onReject = {() => handleReject(driver.driverId)}
-                    />
-                )))
-            }
         </div>
     )
 }
