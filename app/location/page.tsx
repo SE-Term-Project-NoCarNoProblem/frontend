6a80bd87517f6f25608ef76c767fb2d897bb3e39
MooYongArin <132063700+MooYongArin@users.noreply.github.com>
2025-11-11 11:48:11 +0700

    feat: implement restrict UI access function

--- app/location/page.tsx
+++ app/location/page.tsx
@@ -791,7 +791,12 @@ export default function Home() {
 					method: "POST",
 				}
 			);
-
+			// Check 403 forbidden from backend
+			if (res.status === 403) {
+				console.warn("Driver not verified. Redirecting...");
+				window.location.href = "/restricted"; // client-side redirect
+				return;
+			}
 			if (!res.ok) {
 				throw new Error("Failed to accept ride request");
 			}
@@ -867,7 +872,12 @@ export default function Home() {
 						method: "DELETE",
 					}
 				);
-
+				// Check 403 forbidden from backend
+				if (res.status === 403) {
+					console.warn("Driver not verified. Redirecting...");
+					window.location.href = "/restricted"; // client-side redirect
+					return;
+				}
 				if (!res.ok) {
 					throw new Error("Failed to cancel ongoing ride");
 				}
@@ -921,7 +931,12 @@ export default function Home() {
 					}),
 				}
 			);
-
+			// Check 403 forbidden from backend
+			if (res.status === 403) {
+				console.warn("Driver not verified. Redirecting...");
+				window.location.href = "/restricted"; // client-side redirect
+				return;
+			}
 			if (!res.ok) {
 				throw new Error("Failed to update ride status");
 			}
