4c5bbd51a88f9ceff71a75a9fad2376fc6e42cce
Krissada Singhakachain <omsin_47@hotmail.com>
2025-11-13 20:26:12 +0700

    revert: removed testing code from main page

--- app/page.tsx
+++ app/page.tsx
@@ -2,19 +2,14 @@ import NavBar from "./components/NavBar";
 import Hero from "./components/Hero";
 import Features from "./components/Features";
 import Footer from "./components/Footer";
-import CancelOverTime from "./components/CancleOverTime";
-import RatingOverTime from "./components/RatingOverTime";
 export default function Home() {
 	return (
 		<main className="min-h-dvh bg-white">
 			<div className="">
-				{/* <NavBar />
+				<NavBar />
 				<Hero />
 				<Features />
-				<Footer /> */}
-				{/* <CancelOverTime driverId="00efb904-f1e1-4ca6-b61a-42444aeb6913" /> */}
-				<RatingOverTime driverId="00efb904-f1e1-4ca6-b61a-42444aeb6913" />
-				
+				<Footer />
 			</div>
 		</main>
 	);
